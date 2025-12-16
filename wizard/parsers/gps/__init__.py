0e8d30a214c3bc25e7d0cb76a696a2b133386912
Francesco Frassinelli <francesco.frassinelli@nina.no>
2025-12-16 17:09:20 +0100

    Add support for Earth&Ocean mGPS-2

--- wizard/parsers/gps/__init__.py
+++ wizard/parsers/gps/__init__.py
@@ -1,18 +1,33 @@
-from .gpx import GPXParser
-from .igotu import PARSERS as IGOUT
-from .catlog import PARSERS as CATLOG
-from .base import PARSERS as BASE
-from .jm import PARSERS as JM 
-from .unknown import PARSERS as UNKNOWN
-from .pathtrack import PARSERS as PATHTRACK
-from .ho11 import PARSERS as HO11
 from .axytrek import PARSERS as AXYTREK
-from .interrex import PARSERS as INTERREX
-from .ornitela import PARSERS as ORNITELA
-from .mataki import PARSERS as MATAKI
+from .base import PARSERS as BASE
+from .blk import PARSERS as BLK
+from .catlog import PARSERS as CATLOG
 from .ecotone import PARSERS as ECOTONE
+from .gpx import GPXParser
+from .ho11 import PARSERS as HO11
+from .igotu import PARSERS as IGOUT
+from .interrex import PARSERS as INTERREX
+from .jm import PARSERS as JM
+from .mataki import PARSERS as MATAKI
+from .ornitela import PARSERS as ORNITELA
+from .pathtrack import PARSERS as PATHTRACK
+from .unknown import PARSERS as UNKNOWN
 
-PARSERS = [
-    GPXParser,
-] + IGOUT + CATLOG + BASE + JM + UNKNOWN + PATHTRACK + \
-    HO11 + AXYTREK + INTERREX + ORNITELA + MATAKI + ECOTONE
+PARSERS = (
+    [
+        GPXParser,
+    ]
+    + IGOUT
+    + CATLOG
+    + BASE
+    + JM
+    + UNKNOWN
+    + PATHTRACK
+    + HO11
+    + AXYTREK
+    + INTERREX
+    + ORNITELA
+    + MATAKI
+    + ECOTONE
+    + BLK
+)
