0d4fef77c1930ac963c9e68f24a58e48fcea95b3
Niccolo Cantu <niccolo.cantu@nina.no>
2026-02-13 14:47:59 +0100

    feat: admin

---
+++ admin/src/dataProvider.ts
@@ -0,0 +1,24 @@
+import postgrestRestProvider from "@raphiniert/ra-data-postgrest";
+import { fetchUtils } from "react-admin";
+import { TOKEN_KEY } from "./authProvider";
+
+const httpClient = (url: string, options: fetchUtils.Options = {}) => {
+  const token = localStorage.getItem(TOKEN_KEY);
+  const headers = new Headers(options.headers);
+
+  if (token) {
+    headers.set("Authorization", `Bearer ${token}`);
+  }
+
+  return fetchUtils.fetchJson(url, { ...options, headers });
+};
+
+const dataProvider = postgrestRestProvider({
+    apiUrl: "/postgrest",
+    httpClient,
+    defaultListOp: "eq",
+    primaryKeys: new Map(),
+    schema: () => "public",
+});
+
+export default dataProvider;
