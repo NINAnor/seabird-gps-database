76c8273069158d590229c36aa64cbebd946f5ce1
Niccolo Cantu <niccolo.cantu@nina.no>
2026-02-17 13:51:01 +0100

    chg: upload only loggers with metadata, change the directory structure

--- wizard/src/seapop_wizard/tasks.py
+++ wizard/src/seapop_wizard/tasks.py
@@ -5,7 +5,7 @@ import schedule
 import typer
 from gps_logger_parser.parser import detect_file
 
-from seapop_wizard.settings import LOGGERS_PATH, PARQUET_PATH
+from seapop_wizard.settings import LOGGERS_PATH
 
 from .settings import log
 
@@ -15,24 +15,28 @@ app = typer.Typer()
 def check_missing():
     log.info("checking...")
     try:
-        # List all files in loggers path
-        logger_files = list(LOGGERS_PATH.iterdir())
-        for s3_file in logger_files:
+        for s3_file in LOGGERS_PATH.glob("**/*"):
+            if s3_file.is_dir():
+                continue
+
             filename = s3_file.name
-            parquet_filename = filename + ".parquet"
-            parquet_path = PARQUET_PATH / parquet_filename
+
+            parquet_path = s3_file.with_suffix(".parquet")
 
             if not parquet_path.exists():
                 log.info(f"{filename} not found in parquets, processing...")
 
                 # Parse and convert
-                parser = detect_file(path=s3_file)
-                log.info("parsed", parser=parser)
-                pq.write_table(
-                    parser.as_table(),
-                    str(parquet_path),
-                    filesystem=parquet_path.fs,
-                )
+                try:
+                    parser = detect_file(path=s3_file)
+                    log.info("parsed", parser=parser)
+                    pq.write_table(
+                        parser.as_table(),
+                        str(parquet_path),
+                        filesystem=parquet_path.fs,
+                    )
+                except NotImplementedError:
+                    log.error(f"Unable to parse {filename}, skipping...")
     except Exception as e:
         log.error(f"Error checking missing files: {e}")
 
