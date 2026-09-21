faf380f5121c691c9b77e9c44ec0a6175bb85df7
Niccolo Cantu <niccolo.cantu@nina.no>
2026-09-21 09:45:27 +0200

    fix: esbuild

--- admin/vite.config.ts
+++ admin/vite.config.ts
@@ -5,4 +5,40 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/admin/',
+  build: {
+    rolldownOptions: {
+      output: {
+        codeSplitting: {
+          groups: [
+            {
+              name: 'react-vendor',
+              test: /node_modules[\\/]react/,
+              priority: 20,
+            },
+            {
+              name: 'ui-vendor',
+              test: /node_modules[\\/]\@mui/,
+              priority: 15,
+            },
+            {
+              name: 'vendor-admin',
+              test: /node_modules[\\/]react-admin/,
+              priority: 12,
+            },
+            {
+              name: 'vendor',
+              test: /node_modules/,
+              priority: 10,
+            },
+            {
+              name: 'common',
+              minShareCount: 2,
+              minSize: 10000,
+              priority: 5,
+            },
+          ],
+        },
+      },
+    }
+  }
 })
